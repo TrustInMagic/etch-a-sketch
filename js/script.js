2179f0d3c38ad72b0df9f267a31c6861cdcc60f2
Tim Ionescu <timionescu@gmail.com>
2023-01-16 21:16:59 +0200

    Fix sketching-board content box sizing

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-// calculates the square size needed to fill up the container given square number and container side
-function getBoxSize(squareNumber, containerSide) {
-  let iSquare = Math.round(Math.sqrt(squareNumber) + 0.49);
-  let squareSize = containerSide / iSquare;
-  return squareSize
+function drawBoxes(boxNumberOnSide){
+  sketchingBoard.style.cssText = `--grid-rows: ${boxNumberOnSide}; --grid-columns: ${boxNumberOnSide};`
+  for (i = 0; i < (boxNumberOnSide ** 2); i++){
+    let box = document.createElement("div");
+    sketchingBoard.appendChild(box).className = "box";
+  }
 }
 
-function drawBoxes(boxNumber, boxSide) {
-  for (let i = 1; i <= boxNumber; i++) {
-    let cloneBox = box.cloneNode(true);
-    sketchingBoard.appendChild(cloneBox);
-    cloneBox.addEventListener("mouseover", () => changeColor(cloneBox));
-    box.style.cssText = `box-sizing: border-box; float: left; margin: 0; padding: 0; background-color: pink`;
+function reset () {
+  boxNumberOnSide = boxNumberInput.value;
+  sketchingBoard.innerHTML = "";
+  drawBoxes(boxNumberOnSide);
+  let boxes = document.querySelectorAll(".box");
+
+  for (let box of boxes) {
+    box.addEventListener("mouseover", () => changeColor(box));
   }
 }
 
@@ -18,20 +21,15 @@ function changeColor(element) {
   element.classList.add("hovered")
 }
 
-const CONTAINER_SIDE = 500;
 let sketchingBoard = document.querySelector(".sketching-board");
 let boxNumberInput = document.querySelector(".slider input");
-let boxNumberInputValue = document.querySelector("#rangeValue");
-let boxNumber = boxNumberInput.value * boxNumberInput.value;
-let box = document.createElement("div");
-let boxSide = getBoxSize(boxNumber, CONTAINER_SIDE);
+let boxNumberOnSide = boxNumberInput.value;
 
-drawBoxes(boxNumber, boxSide);
+drawBoxes(boxNumberOnSide);
+let boxes = document.querySelectorAll(".box");
 
-boxNumberInput.addEventListener("input", (e) => {
-  boxNumber = boxNumberInput.value * boxNumberInput.value;
-  boxSide = getBoxSize(boxNumber, CONTAINER_SIDE);
-  sketchingBoard.innerHTML = ""
-  drawBoxes(boxNumber, boxSide)
-})
+boxNumberInput.addEventListener("input", () => reset())
 
+for (let box of boxes) {
+  box.addEventListener("mouseover", () => changeColor(box));
+}
